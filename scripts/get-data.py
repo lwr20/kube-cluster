0d6a436e482b9141697ae4603070bc473379c547
Casey Davenport <davenport.cas@gmail.com>
2016-04-04 12:53:02 -0700

    Improve get-data script.

--- scripts/get-data.py
+++ scripts/get-data.py
@@ -1,6 +1,6 @@
 import os
 import numpy
-import pylab
+import matplotlib.pyplot as pylab
 import json
 import subprocess
 import re
@@ -8,10 +8,11 @@ from dateutil import parser
 from subprocess import check_output
 
 # Time extract regex.
-time_re = re.compile("(\d\d:\d\d:\d\d)")
+start_re = re.compile("Started: ([0-9]+\.[0-9]+)")
+end_re = re.compile("Completed: ([0-9]+\.[0-9]+)")
 
 # Stores mapping of start time to elapsed time.
-mapping = {}
+elapsed_by_start_time = {}
 
 # Get all pod names.
 all_pods = check_output(["kubectl", "get", "pods", "-o", "json"])
@@ -25,48 +26,54 @@ pod_names = [p["metadata"]["name"] for p in all_pods
 failed = []
 
 # Arrays for scatter.
-x = []
-y = []
+start_times = []
+elapsed_times = []
+end_times = []
 
 # For each pod, get its logs.
 for p in pod_names:
     logs = check_output(["kubectl", "logs", p])
-    times = time_re.findall(logs)
     try:
-        start_time = times[0]
-        end_time = times[1]
+        start_time = float(start_re.findall(logs)[0])
+        end_time = float(end_re.findall(logs)[0])
     except IndexError:
         print "WARN: Missing start / end time"
         print logs
-        failed.appen((p, logs))
+        failed.append((p, logs))
         continue
 
     # Determine the elapsed time and store in the mapping dict.
-    elapsed = parser.parse(end_time) - parser.parse(start_time)
-    times = mapping.setdefault(start_time, [])
-    times.append(elapsed.seconds)
-    x.append(parser.parse(start_time))
-    y.append(elapsed.seconds)
+    elapsed = end_time - start_time 
+    times = elapsed_by_start_time.setdefault(start_time, [])
+    times.append(elapsed)
 
-starts = mapping.keys()
-starts = sorted(starts, key=lambda d: map(int, str(d).split(':')))
+    # Store the X,Y arrays - start time, elapsed time, respectively.
+    start_times.append(start_time)
+    elapsed_times.append(elapsed)
+    end_times.append(end_time)
 
-for s in starts:
-    print s, mapping[s]
+# Sort all of the start times into an array.
+ordered_start_times = sorted(start_times) 
+ordered_end_times = sorted(end_times) 
+
+for s in ordered_start_times:
+    print s, elapsed_by_start_time[s]
 
 print "%s failed to get logs" % len(failed)
 
 # Create histogram.
 vals = []
-for _, l in mapping.iteritems():
+for _, l in elapsed_by_start_time.iteritems():
     vals += l
 pylab.hist(vals)
 pylab.xlabel('time-to-connectivity')
 pylab.show()
 
-# Scatter plot - time vs start time.
-# Change X into seconds from first.
-min_x = starts[0]
-x = [(i-parser.parse(min_x)).seconds for i in x]
-pylab.plot(x, y, 'ro')
+# Calculate start times, shifted to account
+# for the first pod to start.
+min_x = ordered_start_times[0]
+x = [(t-min_x) for t in start_times]
+
+# Plot data.
+pylab.plot(x, elapsed_times, 'ro')
 pylab.show()
